6a76986afec598e02cb1bd37597ab52a1e0344b8
LostMoa <life4458@naver.com>
2023-02-19 19:52:25 +0900

    20230219 update
    어쌔신 코드 작성

--- Assets/Scripts/Stage/Character/Assassin.cs
+++ Assets/Scripts/Stage/Character/Assassin.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Assassin : MonoBehaviour
+public class Assassin : PlayerCharacter
 {
-    // Start is called before the first frame update
-    void Start()
+    float cooltimeCheck = 0f;
+    bool canAttack = true;
+    [SerializeField] BoxCollider2D weaponCollider;
+
+    private void Update()
     {
-        
+        if (!IsEnemyChecked)
+        {
+            Move();
+        }
+        else if (IsEnemyChecked && canAttack)
+        {
+            StartCoroutine(Attack());
+        }
+        CheckAttackCooltime();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CheckAttackCooltime()
     {
-        
+        if (cooltimeCheck > AttackCooltime)
+        {
+            cooltimeCheck = 0f;
+            canAttack = true;
+        }
+        else
+        {
+            cooltimeCheck += Time.deltaTime;
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        animator.SetTrigger(0);
+        yield return null;
+        float waitTime = animator.GetCurrentAnimatorStateInfo(0).length / 2f;
+        yield return new WaitForSeconds(waitTime);
+        weaponCollider.enabled = true;
     }
 }
