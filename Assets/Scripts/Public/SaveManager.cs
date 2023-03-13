1b08b59f389ff00316681f39c9bdf5dfc814b5d1
LostMoa <life4458@naver.com>
2023-03-13 19:40:35 +0900

    df
    df

---
+++ Assets/Scripts/Public/SaveManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void Start()
+    {
+    }
+}
