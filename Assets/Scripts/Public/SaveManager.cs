bebb273e6177aac137c6ba38de43b8dd6f876c13
LostMoa <life4458@naver.com>
2023-03-22 20:13:00 +0900

    20230322 update
    json으로 저장하기 도전

--- Assets/Scripts/Public/SaveManager.cs
+++ Assets/Scripts/Public/SaveManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
     public static SaveManager instance;
+
+    const string jsonPath = "Assets/Data.json";
 
     private void Awake()
     {
@@ -19,6 +22,34 @@ public class SaveManager : MonoBehaviour
         }
     }
 
+    void JsonSave() // gold, maxStage, 캐릭터 레벨 저장
+    {
+        Debug.Log("HI1");
+        Data data = new();
+        if (!File.Exists(jsonPath))
+        {
+            File.Create(jsonPath);
+        }
+        Debug.Log("HI2");
+        for (int i = 0; i < TeamManager.instance.CharacterDatas.Count; i++)
+        {
+            data.characterLevelList.Add(TeamManager.instance.CharacterDatas.GetValue((Enums.CHAR_TYPE)i).Level);
+        }
+        Debug.Log("HI3");
+        data.gold = GameManager.instance.Gold;
+        data.maxStage = GameManager.instance.MaxStage;
+        Debug.Log("HI4");
+        File.WriteAllText(jsonPath, JsonUtility.ToJson(data), System.Text.Encoding.UTF8);
+        Debug.Log("HI5");
+    }
+
+    class Data
+    {
+        public List<int> characterLevelList = new();
+        public int gold;
+        public int maxStage;
+    }
+
     public void SetData(string key, int value)
     {
         PlayerPrefs.SetInt(key, value);
@@ -31,6 +62,7 @@ public class SaveManager : MonoBehaviour
 
     public void SaveData()
     {
+        JsonSave();
         PlayerPrefs.Save();
     }
 
